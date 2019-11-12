2c8792fb08a514284e0f9504a63c2b6931cefbd1
Karp_Ethan <karpe375@hsd.k12.or.us>
2019-11-12 13:09:16 -0800

    initial files

---
+++ Scripts/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+const UP = Vector2(0 , -1)
+
+func _physics_process(delta):
+	
+	motion.y += 10
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+
+	else:
+		motion.x= 0
+
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -400
+
+	motion = move_and_slide(motion, UP)
+	pass
