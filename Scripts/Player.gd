984cd159b3119e6082d89bf79c787eb0a36bea8b
Karp_Ethan <karpe375@hsd.k12.or.us>
2019-11-14 13:50:06 -0800

    changed vertical movement to single button

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,36 +1,53 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-var grav : int = 10
+var grav : float = 1
+var up_down : int = 0
 
 const UP = Vector2(0 , -1)
+
+
 
 func _physics_process(delta):
 	
 	motion.y += grav
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
-		$Sprite.flip_h = true
-
+		motion.x = 150
+		$body.flip_h = false
+		$leg.flip_h = false
+		$leg2.flip_h = false
+		$arm.flip_h = false
+		$arm2.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
-		$Sprite.flip_h = true
-
+		motion.x = -150
+		$body.flip_h = true
+		$leg.flip_h = true
+		$leg2.flip_h = true
+		$arm.flip_h = true
+		$arm2.flip_h = true
 	else:
 		motion.x= 0
 
-	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			motion.y = -400
-			$Sprite.flip_v = true
-			grav = -10
 
-	if is_on_ceiling():
-		if Input.is_action_pressed("ui_down"):
-			motion.y = 400
-			$Sprite.flip_v = false
-			grav = 10
+	if Input.is_action_pressed("spacebar"):
+		if is_on_floor():
+			motion.y = -100
+			$body.flip_v = true
+			$leg.flip_v = true
+			$leg2.flip_v = true
+			$arm.flip_v = true
+			$arm2.flip_v = true
+			grav = -5
+
+		if is_on_ceiling():
+			motion.y = 100
+			$body.flip_v = false
+			$leg.flip_v = false
+			$leg2.flip_v = false
+			$arm.flip_v = false
+			$arm2.flip_v = false
+			grav = 5
 
 	motion = move_and_slide(motion, UP)
 	pass
