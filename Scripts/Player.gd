a2e6a75628f7f6ada3ffa85e1acd9a28cfbd7474
Karp_Ethan <karpe375@hsd.k12.or.us>
2019-12-04 13:39:21 -0800

    Legs now flip

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,11 +2,14 @@ extends KinematicBody2D
 
 var motion = Vector2()
 var grav : float = 1
-var up_down : int = 0
 
 const UP = Vector2(0 , -1)
 
-
+func _ready():
+	$rev_leg.hide()
+	$rev_leg2.hide()
+	$rev_arm.hide()
+	$rev_arm2.hide()
 
 func _physics_process(delta):
 	
@@ -31,26 +34,34 @@ func _physics_process(delta):
 		$AnimationPlayer.play("walk")
 	else:
 		motion.x= 0
-		$AnimationPlayer.play("idle")
+		$AnimationPlayer.stop(true)
 
 
 	if Input.is_action_pressed("spacebar"):
 		if is_on_floor():
 			motion.y = -100
 			$body.flip_v = true
-			$leg.flip_v = true
-			$leg2.flip_v = true
-			$arm.flip_v = true
-			$arm2.flip_v = true
+			$leg.hide()
+			$leg2.hide()
+			$arm.hide()
+			$arm2.hide()
+			$rev_leg.show()
+			$rev_leg2.show()
+			$rev_arm.show()
+			$rev_arm2.show()
 			grav = -5
 
 		if is_on_ceiling():
 			motion.y = 100
 			$body.flip_v = false
-			$leg.flip_v = false
-			$leg2.flip_v = false
-			$arm.flip_v = false
-			$arm2.flip_v = false
+			$leg.show()
+			$leg2.show()
+			$arm.show()
+			$arm2.show()
+			$rev_leg.hide()
+			$rev_leg2.hide()
+			$rev_arm.hide()
+			$rev_arm2.hide()
 			grav = 5
 
 	motion = move_and_slide(motion, UP)
