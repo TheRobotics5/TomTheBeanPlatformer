2190269ea4f11ba89a74093a3d81636a1005c753
Ethan Karp <ethankarp5@gmail.com>
2019-12-18 20:23:15 -0800

    can animation

--- Scripts/Entrance.gd
+++ Scripts/Entrance.gd
@@ -2,10 +2,23 @@ extends Area2D
 
 var scenename : String = "none"
 
+var win = preload("res://Scripts/win_var.gd")
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
 			scenename = get_tree().get_current_scene().get_name()
-			get_tree().change_scene("res://Levels/Level2.tscn")
 			print (scenename)
+			$"../AnimationPlayer".play("entered")
+			$"../AnimationPlayer".queue("none")
+			$"../Animation".start()
+			win = 1
+
+
+func _on_Animation_timeout():
+	win = 0
+	if scenename == "Level1":
+		get_tree().change_scene("res://Levels/Level2.tscn")
+	elif scenename == "Level2":
+		get_tree().change_scene("res://Levels/Level3.tscn")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,9 @@ var grav : float = 3.5
 var walk_speed : int = 250
 var respawnPoint
 
+# var win = preload("res://Scripts/win_var.gd")
+var win = 0
+
 const UP = Vector2(0 , -1)
 
 func _ready():
@@ -22,55 +25,58 @@ func _ready():
 	$arm2.show()
 	$AnimationPlayer.play("idle")
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
 	
 	motion.y += grav
 	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = 250
-		$body.flip_h = false
-		$rev_body.flip_h = false
-		$AnimationPlayer.play("walk")
-		
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -walk_speed
-		$body.flip_h = true
-		$rev_body.flip_h = true
-		$AnimationPlayer.play("walk")
-	else:
-		motion.x= 0
-		$AnimationPlayer.play("idle")
+	if win == 0:
+		if Input.is_action_pressed("ui_right"):
+			motion.x = 250
+			$body.flip_h = false
+			$rev_body.flip_h = false
+			$AnimationPlayer.play("walk")
+			
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -walk_speed
+			$body.flip_h = true
+			$rev_body.flip_h = true
+			$AnimationPlayer.play("walk")
+		else:
+			motion.x= 0
+			$AnimationPlayer.play("idle")
+	
+		if Input.is_action_pressed("spacebar"):
+			if is_on_floor():
+				motion.y = -walk_speed
+				$body.hide()
+				$leg.hide()
+				$leg2.hide()
+				$arm.hide()
+				$arm2.hide()
+				$rev_body.show()
+				$rev_leg.show()
+				$rev_leg2.show()
+				$rev_arm.show()
+				$rev_arm2.show()
+				grav = -8
+	
+			if is_on_ceiling():
+				motion.y = 100
+				$body.show()
+				$leg.show()
+				$leg2.show()
+				$arm.show()
+				$arm2.show()
+				$rev_body.hide()
+				$rev_leg.hide()
+				$rev_leg2.hide()
+				$rev_arm.hide()
+				$rev_arm2.hide()
+				grav = 8
+		motion = move_and_slide(motion, UP)
 
-	if Input.is_action_pressed("spacebar"):
-		if is_on_floor():
-			motion.y = -walk_speed
-			$body.hide()
-			$leg.hide()
-			$leg2.hide()
-			$arm.hide()
-			$arm2.hide()
-			$rev_body.show()
-			$rev_leg.show()
-			$rev_leg2.show()
-			$rev_arm.show()
-			$rev_arm2.show()
-			grav = -8
-
-		if is_on_ceiling():
-			motion.y = 100
-			$body.show()
-			$leg.show()
-			$leg2.show()
-			$arm.show()
-			$arm2.show()
-			$rev_body.hide()
-			$rev_leg.hide()
-			$rev_leg2.hide()
-			$rev_arm.hide()
-			$rev_arm2.hide()
-			grav = 8
-	motion = move_and_slide(motion, UP)
-
+# warning-ignore:unused_argument
 func _on_Limits_area_exited(area):
 	print ("died")
 	global_transform = respawnPoint
