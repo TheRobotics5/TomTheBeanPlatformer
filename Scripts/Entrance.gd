086f09e49b7cd6b2c51ba72d44524d6371728fac
Karp_Ethan <karpe375@hsd.k12.or.us>
2020-01-13 13:47:40 -0800

    Fixed bug, can now switch between levels easily

--- Scripts/Entrance.gd
+++ Scripts/Entrance.gd
@@ -1,22 +1,21 @@
 extends Area2D
 
 var scenename : String = "none"
-var win = 0
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
+			$Camera2D.make_current()
 			scenename = get_tree().get_current_scene().get_name()
 			$"../AnimationPlayer".play("entered")
 			$"../Animation".start()
-			win = 1
+
 
 
 func _on_Animation_timeout():
-	win = 0
 	if scenename == "Level1":
 		get_tree().change_scene("res://Levels/Level2.tscn")
-	elif scenename == "Level2":
+	if scenename == "Level2":
 		get_tree().change_scene("res://Levels/Level3.tscn")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,7 @@ var motion = Vector2()
 var grav : float = 3.5
 var walk_speed : int = 250
 var respawnPoint
+var win = 0
 
 
 const UP = Vector2(0 , -1)
@@ -27,52 +28,53 @@ func _ready():
 # warning-ignore:unused_argument
 func _physics_process(delta):
 
-	motion.y += grav
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x = 250
-		$body.flip_h = false
-		$rev_body.flip_h = false
-		$AnimationPlayer.play("walk")
-
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -walk_speed
-		$body.flip_h = true
-		$rev_body.flip_h = true
-		$AnimationPlayer.play("walk")
-	else:
-		motion.x= 0
-		$AnimationPlayer.play("idle")
-
-	if Input.is_action_pressed("spacebar"):
-		if is_on_floor():
-			motion.y = -walk_speed
-			$body.hide()
-			$leg.hide()
-			$leg2.hide()
-			$arm.hide()
-			$arm2.hide()
-			$rev_body.show()
-			$rev_leg.show()
-			$rev_leg2.show()
-			$rev_arm.show()
-			$rev_arm2.show()
-			grav = -8
-
-		if is_on_ceiling():
-			motion.y = 100
-			$body.show()
-			$leg.show()
-			$leg2.show()
-			$arm.show()
-			$arm2.show()
-			$rev_body.hide()
-			$rev_leg.hide()
-			$rev_leg2.hide()
-			$rev_arm.hide()
-			$rev_arm2.hide()
-			grav = 8
-	motion = move_and_slide(motion, UP)
+	if win == 0:
+		motion.y += grav
+	
+		if Input.is_action_pressed("ui_right"):
+			motion.x = 250
+			$body.flip_h = false
+			$rev_body.flip_h = false
+			$AnimationPlayer.play("walk")
+	
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -walk_speed
+			$body.flip_h = true
+			$rev_body.flip_h = true
+			$AnimationPlayer.play("walk")
+		else:
+			motion.x= 0
+			$AnimationPlayer.play("idle")
+	
+		if Input.is_action_pressed("spacebar"):
+			if is_on_floor():
+				motion.y = -walk_speed
+				$body.hide()
+				$leg.hide()
+				$leg2.hide()
+				$arm.hide()
+				$arm2.hide()
+				$rev_body.show()
+				$rev_leg.show()
+				$rev_leg2.show()
+				$rev_arm.show()
+				$rev_arm2.show()
+				grav = -8
+	
+			if is_on_ceiling():
+				motion.y = 100
+				$body.show()
+				$leg.show()
+				$leg2.show()
+				$arm.show()
+				$arm2.show()
+				$rev_body.hide()
+				$rev_leg.hide()
+				$rev_leg2.hide()
+				$rev_arm.hide()
+				$rev_arm2.hide()
+				grav = 8
+		motion = move_and_slide(motion, UP)
 
 # warning-ignore:unused_argument
 func _on_Limits_area_exited(area):
@@ -80,6 +82,7 @@ func _on_Limits_area_exited(area):
 	global_transform = respawnPoint
 
 
-func _on_Entrance_area_entered(area):
-	print ("won")
-	queue_free()
+func _on_Entrance_body_entered(body):
+	if (body.get_name() == "Player"):
+		queue_free()
+		win = 1
